77bf48cf85a3729067a6f240ca67f94053fd677e
Deff <flus7107@gmail.com>
2026-07-07 11:12:43 +0300

    Full completed cube

--- 3D spinning cube.py
+++ 3D spinning cube.py
@@ -38,17 +38,22 @@ class Cube:
         self.Z = 0
         self.edge_points = []
         self.vertices = []
-        self.cube_faces = []
-        self.screen_verticesC = []
+        self.cube_facesC = []
+        self.screen_facesC = []
+        self.cubes_to_draw = []
+        self.faces_to_draw = []
+        self.face_vertexesSCR = []
         
     def draw(self):
+        self.faces_to_draw = []
         self.screen_vertices_dat = []
         self.edge_points = []
-        self.screen_verticesC = []
+        self.screen_facesC = []
         self.rot = (lerp(-pi, pi, (sin(t)+1)/2), lerp(-pi, pi, (sin(t+6)+1)/2), lerp(-pi, pi, (cos(t+2)+1)/2))
         self.xR = self.rot[0]
         self.yR = self.rot[1]
         self.zR = self.rot[2]
+        self.face_vertexesSCR = []
         self.matrix = [[cos(self.yR)*cos(self.zR), sin(self.xR)*sin(self.yR)*cos(self.zR)-cos(self.xR)*sin(self.zR), cos(self.xR)*sin(self.yR)*cos(self.zR)+sin(self.xR)*sin(self.zR)],
                        [cos(self.yR)*sin(self.zR), sin(self.xR)*sin(self.yR)*sin(self.zR)+cos(self.xR)*cos(self.zR), cos(self.xR)*sin(self.yR)*sin(self.zR)-sin(self.xR)*cos(self.zR)],
                        [-sin(self.yR), sin(self.xR)*cos(self.yR), cos(self.xR)*cos(self.yR)]]
@@ -72,11 +77,28 @@ class Cube:
 [(self.vc[5][0]*self.matrix[0][0]) + (self.vc[5][1]*self.matrix[0][1]) + (self.vc[5][2]*self.matrix[0][2]), ((self.vc[5][0]*self.matrix[1][0]) + (self.vc[5][1]*self.matrix[1][1]) + (self.vc[5][2]*self.matrix[1][2])), (self.vc[5][0]*self.matrix[2][0]) + (self.vc[5][1]*self.matrix[2][1]) + (self.vc[5][2]*self.matrix[2][2])],
 [(self.vc[6][0]*self.matrix[0][0]) + (self.vc[6][1]*self.matrix[0][1]) + (self.vc[6][2]*self.matrix[0][2]), ((self.vc[6][0]*self.matrix[1][0]) + (self.vc[6][1]*self.matrix[1][1]) + (self.vc[6][2]*self.matrix[1][2])), (self.vc[6][0]*self.matrix[2][0]) + (self.vc[6][1]*self.matrix[2][1]) + (self.vc[6][2]*self.matrix[2][2])],
 [(self.vc[7][0]*self.matrix[0][0]) + (self.vc[7][1]*self.matrix[0][1]) + (self.vc[7][2]*self.matrix[0][2]), ((self.vc[7][0]*self.matrix[1][0]) + (self.vc[7][1]*self.matrix[1][1]) + (self.vc[7][2]*self.matrix[1][2])), (self.vc[7][0]*self.matrix[2][0]) + (self.vc[7][1]*self.matrix[2][1]) + (self.vc[7][2]*self.matrix[2][2])]]
-  
+        
+        self.cube_facesC = [(self.vertices[0], self.vertices[1], self.vertices[2], self.vertices[3]),
+                           (self.vertices[4], self.vertices[5], self.vertices[6], self.vertices[7]),
+                           (self.vertices[0], self.vertices[1], self.vertices[5], self.vertices[4]),
+                           (self.vertices[2], self.vertices[3], self.vertices[7], self.vertices[6]),
+                           (self.vertices[0], self.vertices[3], self.vertices[7], self.vertices[4]),
+                           (self.vertices[1], self.vertices[2], self.vertices[6], self.vertices[5])]
+        
         for i in self.vertices:
             screenXYZ = i[0] * focal_length / distance(0,camera_pos[1],0,0,i[1],0, True, "y") + hweight, i[2] * focal_length / distance(0,camera_pos[1],0,0,i[1],0, True, "y") + hheight
             self.screen_vertices_dat.append((screenXYZ[0], screenXYZ[1], distance(0,camera_pos[1],0,0,i[1],0, True, "y"), i[1]))
-            self.screen_verticesC.append((screenXYZ[0], screenXYZ[1]))
+            
+
+        for i in self.cube_facesC:
+            faceZ = distance(0,camera_pos[1],0,0,(i[0][1]+i[2][1])/2,0, True, "y")
+            facesscreenXYZ = ((i[0][0]+i[2][0])/2) * focal_length / faceZ + hweight, ((i[0][2] + i[2][2])/2) * focal_length / faceZ + hheight, faceZ
+            self.screen_facesC.append((faceZ, facesscreenXYZ[0], facesscreenXYZ[1], self.cube_facesC.index(i)))
+            coords = []
+            for j in i:
+                coords.append(((j[0]) * focal_length / distance(0,camera_pos[1],0,0,j[1],0, True, "y") + hweight, (j[2]) * focal_length / distance(0,camera_pos[1],0,0,j[1],0, True, "y") + hheight))
+
+            self.face_vertexesSCR.append(tuple(coords))
 
         for i in self.vertices:
             self.edge_points.append((i[0], i[1]))
@@ -85,14 +107,6 @@ class Cube:
             colorvert = max(min(255, round(lerp(1, 0.2, (i[3]/yblackout)) * 255)), 0)
             pygame.draw.circle(screen, rgb_unit0_1(colorvert, False), (i[0], i[1]), 5)
 
-        self.cube_faces = [(self.screen_verticesC[0], self.screen_verticesC[1], self.screen_verticesC[2], self.screen_verticesC[3]),
-                           (self.screen_verticesC[4], self.screen_verticesC[5], self.screen_verticesC[6], self.screen_verticesC[7]),
-                           (self.screen_verticesC[0], self.screen_verticesC[1], self.screen_verticesC[5], self.screen_verticesC[4]),
-                           (self.screen_verticesC[2], self.screen_verticesC[3], self.screen_verticesC[7], self.screen_verticesC[6]),
-                           (self.screen_verticesC[0], self.screen_verticesC[3], self.screen_verticesC[7], self.screen_verticesC[4]),
-                           (self.screen_verticesC[1], self.screen_verticesC[2], self.screen_verticesC[6], self.screen_verticesC[5]),                        
-                           ]
-        
         for i in range(8):
             coloredge = max(min(255, round(lerp(1, 0.2, (self.screen_vertices_dat[i][3]/(yblackout+1))) * 255)), 0)
             start = (self.screen_vertices_dat[i][0], self.screen_vertices_dat[i][1])
@@ -105,14 +119,12 @@ class Cube:
                 start = (self.screen_vertices_dat[i-4][0], self.screen_vertices_dat[i-4][1])
                 end = (self.screen_vertices_dat[i][0], self.screen_vertices_dat[i][1])
                 pygame.draw.line(screen, rgb_unit0_1(coloredge, False), start, end)
-
-        colorface = 0
-
-        points = []
-        for i in self.cube_faces: 
-            colorface = max(min(255, round(lerp(1, 0.2, (self.screen_vertices_dat[self.cube_faces.index(i)][3] /(yblackout+1))) * 255)), 0)
-            pygame.draw.polygon(screen, rgb_unit0_1(colorface, False), i)
-
+    
+        self.screen_facesC.sort(reverse=True)
+        for c in self.screen_facesC:
+            colorface = coloredge = max(min(255, round(lerp(1, 0.2, (c[0]/(yblackout+4))) * 255)), 0)
+            elem = self.face_vertexesSCR[c[3]]
+            pygame.draw.polygon(screen, rgb_unit0_1(colorface, False), (elem[0], elem[1], elem[2], elem[3]))
 
 cube = Cube(7, (0, 0, 0))        
 cube.draw()
